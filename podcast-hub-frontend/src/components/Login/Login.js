3135087d3401a9749d56cf5434b4da0f2f5a22fb
Prathamesh Dantkale <prathameshdantkale@Prathameshs-MacBook-Pro.local>
2023-07-03 20:57:57 +0530

    second commit

--- podcast-hub-frontend/src/components/Login/Login.js
+++ podcast-hub-frontend/src/components/Login/Login.js
@@ -1,75 +1,70 @@
-// Login.jsx
-import React, { useState } from 'react';
-import './Login.css';
+// // Login.jsx
+// import React, { useState } from 'react';
+// import './Login.css';
 
-const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+// const Login = () => {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [error, setError] = useState('');
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
+//   const handleUsernameChange = (e) => {
+//     setUsername(e.target.value);
+//   };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+//   const handlePasswordChange = (e) => {
+//     setPassword(e.target.value);
+//   };
 
-  // const handleSignUp = (e) => {
-  //   e.preventDefault();
-  //   // Implement your sign up logic here
-  //   console.log('Sign up clicked');
-  // };
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
 
-    try {
-      const response = await fetch('http://localhost:3000/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
+//     try {
+//       const response = await fetch('http://localhost:3000/login', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ username, password }),
+//       });
 
-      if (response.ok) {
-        // Successful login
-        console.log('Login successful');
-      } else {
-        // Invalid credentials
-        const errorData = await response.json();
-        setError(errorData.message);
-      }
-    } catch (error) {
-      console.error('Login failed:', error);
-    }
-  };
+//       if (response.ok) {
+//         // Successful login
+//         console.log('Login successful');
+//       } else {
+//         // Invalid credentials
+//         const errorData = await response.json();
+//         setError(errorData.message);
+//       }
+//     } catch (error) {
+//       console.error('Login failed:', error);
+//     }
+//   };
 
-  return (
-    <div className="login-container">
-      <h1>Welcome to PodcastHub</h1>
-      {error && <p className="error-message">{error}</p>}
-      <form onSubmit={handleLogin}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={handleUsernameChange}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={handlePasswordChange}
-          required
-        />
-        <button type="submit">Log In</button>
-      <button type="submit" style={{margin:"10px"}}>Sign up</button>
-      </form>
-    </div>
-  );
-};
+//   return (
+//     <div className="login-container">
+//       <h1>Welcome to PodcastHub</h1>
+//       {error && <p className="error-message">{error}</p>}
+//       <form onSubmit={handleLogin}>
+//         <input
+//           type="text"
+//           placeholder="Username"
+//           value={username}
+//           onChange={handleUsernameChange}
+//           required
+//         />
+//         <input
+//           type="password"
+//           placeholder="Password"
+//           value={password}
+//           onChange={handlePasswordChange}
+//           required
+//         />
+//         <button type="submit">Log In</button>
+//       <button type="submit" style={{margin:"10px"}}>Sign up</button>
+//       </form>
+//     </div>
+//   );
+// };
 
-export default Login;
+// export default Login;
